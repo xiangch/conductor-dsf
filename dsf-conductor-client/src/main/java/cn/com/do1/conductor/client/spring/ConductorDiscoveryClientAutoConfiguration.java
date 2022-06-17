617c08636e8592ee7da70eff56988c71d6859ecf
曾祥超 <zengxiangchao@do1.com.cn>
2022-06-17 18:52:14 +0800

    删除FastJsonHttpMessageConverter这里与七巧P冲突导致重复引用

--- dsf-conductor-client/src/main/java/cn/com/do1/conductor/client/spring/ConductorDiscoveryClientAutoConfiguration.java
+++ dsf-conductor-client/src/main/java/cn/com/do1/conductor/client/spring/ConductorDiscoveryClientAutoConfiguration.java
@@ -35,44 +35,6 @@ public class ConductorDiscoveryClientAutoConfiguration {
     @Autowired(required = false)
     private List<Worker> workers = new ArrayList<>();
 
-
-    @ConditionalOnMissingBean
-    @Bean
-    FastJsonHttpMessageConverter fastJsonHttpMessageConverter() {
-        //1、定义一个convert转换消息的对象
-        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
-
-        //2:添加fastJson的配置信息
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(
-            // 是否输出值为null的字段,默认为false
-            SerializerFeature.WriteMapNullValue,
-            // 将Collection类型字段的字段空值输出为[]
-            SerializerFeature.WriteNullListAsEmpty,
-            // 将字符串类型字段的空值输出为空字符串
-            SerializerFeature.WriteNullStringAsEmpty,
-            // 将数值类型字段的空值输出为0
-            SerializerFeature.WriteNullNumberAsZero,
-            //Boolean字段如果为null,输出为false,而非null
-            SerializerFeature.WriteNullBooleanAsFalse,
-            SerializerFeature.WriteDateUseDateFormat,
-            //枚举字段输出为枚举值
-            SerializerFeature.WriteEnumUsingToString,
-            // 禁用循环引用
-            SerializerFeature.DisableCircularReferenceDetect);
-        //这个日期格式是全局格式：yyyy-MM-dd HH:mm:ss
-        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
-        fastJsonConfig.setCharset(StandardCharsets.UTF_8);
-
-        //3处理中文乱码问题
-        List<MediaType> fastMediaTypes = new ArrayList<>();
-        fastMediaTypes.add(MediaType.APPLICATION_JSON);
-        //4.在convert中添加配置信息.
-        converter.setSupportedMediaTypes(fastMediaTypes);
-        converter.setFastJsonConfig(fastJsonConfig);
-        return converter;
-    }
-
     @Bean
     public TaskClient taskClient(TaskFeignClient feignClient) {
         return new DiscoveryTaskClient(feignClient);
